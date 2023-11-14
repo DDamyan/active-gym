dc096a70edc8cf40ccddd3ee49d50833b9230ce1
Damyan Damyanov <ddminedd@gmail.com>
2023-11-14 19:07:48 +0100

    ✍ server$: appendFileSync

--- src/routes/me/index.tsx
+++ src/routes/me/index.tsx
@@ -1,5 +1,16 @@
 import { $, component$, useSignal } from "@builder.io/qwik";
-import { log } from "console";
+import { server$ } from "@builder.io/qwik-city";
+import { appendFileSync } from "fs";
+
+const doubleIt = server$((counter: number) => {
+  const doubleValue = Math.pow(counter, 2);
+  console.log("doubleValue", doubleValue);
+  appendFileSync(
+    "C:\\Users\\ddmin\\Desktop\\Source\\active-gym\\src\\routes\\me\\test.txt",
+    "\n" + doubleValue.toString(),
+  );
+  return doubleValue;
+});
 
 export default component$(() => {
   const counter = useSignal(0);
@@ -16,6 +27,11 @@ export default component$(() => {
       <h3>{counter.value}</h3>
       <button onClick$={() => setCount(counter.value + 1)}>+</button>
       <button onClick$={() => setCount(counter.value - 1)}>-</button>
+      <div>
+        <button onClick$={async () => setCount(await doubleIt(counter.value))}>
+          Double It!
+        </button>
+      </div>
     </div>
   );
 });
